8e76b0c26cbf49345674af69dc4884fa147943aa
Antonio Matteucci <abmatteucci@gmail.com>
2024-02-04 15:19:34 +0000

    Topic8 quizz

---
+++ JFT8/quizz.java
@@ -0,0 +1,131 @@
+package JFT8;
+
+public class quizz {
+
+    public static void main(String[] args) {
+        
+        /*
+        int luckyNumber7 = 7;
+        int[] numbers = {8,7,1};
+
+        for(int x=0; x<numbers.length; x++){
+            if(numbers[x] == luckyNumber7){
+                System.out.println("Lucky number 7 found!"); 
+                break;
+            }
+            System.out.println("The program will keep searching."); 
+        }
+        System.out.println("Program closing."); 
+        */
+
+        /*
+        int[] numbers = new int[] {1,2,3,4};
+
+        for(int x=0; x<numbers.length; x++){
+
+            System.out.println(numbers[x]);
+        }
+        */
+
+        /*
+        float[] studentGrades = new float[5];
+
+        final float passMark = 40.00f;
+        String passedExamMessage = "Well done, you passed the exam!";
+
+        studentGrades[0] = 25.00f;
+        studentGrades[1] = 49.00f;
+        studentGrades[2] = 55.00f;
+        studentGrades[3] = 65.00f;
+        studentGrades[4] = 12.00f;
+
+        for(int i=0; i<studentGrades.length; i++){
+            if(studentGrades[i] < passMark){
+                continue;
+            }
+            System.out.println(passedExamMessage);
+        }
+
+        */
+
+        /*
+        for(int x=0; x<15; x+=6){
+
+            System.out.println(x);
+          
+        }
+        */
+
+        /*
+        for(int x=0; x<5; x--){
+            System.out.println(x);
+        
+        }
+        */
+
+        /*
+        int counter = 1;
+
+        while(counter < 5){
+            System.out.println(++counter);
+        }
+
+        */
+         /*
+        int counter = 1;
+
+        do{
+            System.out.println(counter++);
+
+        }while(counter < 5);
+        */
+
+         /*
+        String[] frutas = new String[5]; // Linha 3
+        String[] vegetais = new String[]{"Cenouras", "Brócolis"}; // Linha 4
+        String[] lanches = {"Batatas fritas","Chocolate"}; // Linha 5
+        String[] drinks = new String[3]{null,"Cola","Orange"}; // Linha 6
+
+        */
+
+        /*
+        float f=2.0f;
+
+        switch(f){
+            case 0:
+            System.out.println("f=0");
+            break;
+            case 1:
+            System.out.println("f=1");
+            break;
+            case 2:
+            System.out.println("f=2");
+            break;
+            default:
+            System.out.println("f does not store the values, 1, 2 or 3.");
+            break;
+            }
+        */
+        /*
+        String result = "The result of the calculation is: " + 10 + 20;
+        System.out.println(result);
+        */
+        /*
+        short a = 14;
+        short b = 13;
+        short c = 14;
+
+        if((a-->=b) & (--c>--a)){
+            System.out.println("Hello World! " + (a+b+c));
+        }else{
+            System.out.println("Hello Universe! " + (a+b+c));
+        }
+        */
+
+        /*
+    // A single line comment can be placed within a multi-line comment.
+   */
+
+    }
+    
+}
